2080dd951538920a1fc594b581e0a4a9911fe512
crc8109 <crc8109@pm.me>
2022-03-22 15:52:41 -0400

    Change prompt to convey more accurate info; add more detail to comments

--- decrypt.py
+++ decrypt.py
@@ -2,23 +2,24 @@ def decrypt(scrambled, ascii, iicsa, key):
     input("\nTo decrypt, we simply take the same 256-bit key we used to "
           "encrypt and run XOR again. This type of encryption is called "
           "symmetrical because the same key is used in encryption and "
-          "decryption. Now let's decrypt.")
+          "decryption. In asymmetrical encryption, you'd use one key to,"
+          "encrypt and another to decrypt. Now let's decrypt.")
 
     input("\nDecrypting...")
-    reversed = ''
-    # Decrypt encrypted message
+    decrypted = ''
+    # Decrypt encrypted message by using XOR (^) again
     for k in range(len(scrambled)):
-        reversed += str(int(scrambled[k]) ^ int(key[k]))
+        decrypted += str(int(scrambled[k]) ^ int(key[k]))
 
-    # create number of needed blocks in array
+    # create number of needed blocks again
     blocks = []
     for block in range(len(scrambled)//8):
         blocks.append('')
 
-    # start filling out each byte of ascii binary
+    # start filling out each byte with the decrypted binary (ASCII values)
     block = 0
-    for letter in range(len(reversed)):
-        blocks[block] += reversed[letter]
+    for letter in range(len(decrypted)):
+        blocks[block] += decrypted[letter]
 
         # parse digits by bytes
         if len(blocks[block]) == 8:
@@ -40,7 +41,7 @@ def decrypt(scrambled, ascii, iicsa, key):
 
     input("\nUsing XOR in encryption is a common technique, but it's not "
           "enough on its own. This is because if you send enough messages, "
-          "people can figure out your key.")
+          "people can figure out your key through guess-and-checking.")
 
     input("\nOne way to counteract this is to use a different key for each "
           "message, but that soon starts to take up a lot of space. You can "
